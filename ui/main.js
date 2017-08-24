f3741ecaf57a285e5489e63d72d7d58ba5725f3e
Neha Jeevan <nehajeevan28@gmail.com>
2017-08-25 00:57:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 //Move the image
 var img = document.getElementById('madi');
-img.onClick = function() {
+img.onclick = function() {
     img.style.marginLeft = "100px";   
 }
