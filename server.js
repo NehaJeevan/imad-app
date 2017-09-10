714474966d8ee5e839f22bac3d04f0f7075d56ef
Neha Jeevan <nehajeevan28@gmail.com>
2017-09-10 20:38:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -115,8 +115,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article/:articleName', function (err, result) {
-  pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName +'"', function(req,res){
+app.get('/article/:articleName', function (req, res) {
+  pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName +'"', function(err,result){
       if(err){
           res.status(500).send(err.toString());
       }
