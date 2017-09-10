90357ffcbeea85cd083df9f44ff3f91438fb1913
Neha Jeevan <nehajeevan28@gmail.com>
2017-09-10 20:50:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ var htmlTemplate = `
             ${heading}
         </h3>
         <div>
-            ${date}
+            ${date.toDateString()}
         </div>
         <div>
             ${content}
