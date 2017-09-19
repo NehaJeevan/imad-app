e5347bbd5edef528578208f207165147a8cfad3f
Neha Jeevan <nehajeevan28@gmail.com>
2017-09-20 02:41:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -147,6 +147,11 @@ app.get('/check-login', function(req,res){
    }
 });
 
+app.get('/logout',function(req,res){
+   delete req.session.auth;
+   res.send('Logged out');
+});
+
 app.get('/articles/:articleName', function (req, res) {
   pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function(err,result){
       if(err){
