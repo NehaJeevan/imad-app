df678b21953c4a765788381fd5cd81df1524f365
Neha Jeevan <nehajeevan28@gmail.com>
2017-09-19 21:27:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -118,7 +118,7 @@ app.post('/login',function(req,res){
         }
         else{
             if(result.rows.length === 0){
-                    res.status(403).send("Username/Password is invalid");
+                    res.send(403).send("Username/Password is invalid");
             }else{
                     var dbString = result.rows[0].password;
                     var salt = dbString.split('$')[2];
