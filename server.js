8752aee711df00d05952b86cadc6c37975e74381
Neha Jeevan <nehajeevan28@gmail.com>
2017-09-19 19:24:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.post('/create-user', function(req,res){
             res.status(500).send(err.toString());
         }
         else{
-            res.send("User succesfully created" + username);
+            res.send("User succesfully created: " + username);
         }
     });
     
